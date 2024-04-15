fa3e98f8d0c68bbf23648b77e3b9757bbe1dc05d
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-15 20:17:04 +0300

    add crud

--- clientlist/app/views.py
+++ clientlist/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from app.models import Client
 from .forms import ClientForm
 
@@ -26,3 +27,28 @@ def add_client(request):
     else:
         form = ClientForm()
     return render(request, 'clients/add_client.html', {'form': form})
+
+
+def edit_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'clients/edit_client.html', {'form': form})
+
+
+def client_detail(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    return render(request, 'clients/client_detail.html', {'client': client})
+
+
+def delete_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client_list')
+    return render(request, 'clients/client_list.html', {'client': client})
--- clientlist/clientlist/urls.py
+++ clientlist/clientlist/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from app.views import client_list
 from app.views import add_client
+from app.views import edit_client
+from app.views import client_detail
+from app.views import delete_client
 from app.views import index
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('list/', client_list, name='client_list'),
     path('add_client/', add_client, name='add_client'),
+    path('list/', client_list, name='client_list'),
+    path('edit_client/<int:client_id>/', edit_client, name='edit_client'),
+    path('client_detail/<int:client_id>/', client_detail, name='client_detail'),
+    path('delete_client/<int:client_id>/', delete_client, name='delete_client'),
 ]
