5723fc7ad60349875034939193166602cfe0b633
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-15 19:50:05 +0300

    add index.html

--- clientlist/app/views.py
+++ clientlist/app/views.py
@@ -4,3 +4,7 @@ from app.models import Client
 def client_list(request):
     clients = Client.objects.all()
     return render(request, 'clients/client_list.html', {'clients': clients})
+
+
+def index(request):
+    return render(request, 'index.html')
